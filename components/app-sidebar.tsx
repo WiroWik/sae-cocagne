48708e799e79e9b11d04b0b3be65dc7da5c60639
WiroWik <wirowik67@gmail.com>
2025-01-12 02:43:12 +0100

    Ajout item sidebar

--- components/app-sidebar.tsx
+++ components/app-sidebar.tsx
@@ -3,25 +3,59 @@ import {
     SidebarContent,
     SidebarFooter,
     SidebarGroup,
+    SidebarGroupContent,
+    SidebarGroupLabel,
     SidebarHeader,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
 } from "@/components/ui/sidebar"
 import Image from "next/image"
 import logoCocagne from "@/public/images/cocagne-vert.png"
 import { Separator } from "@/components/ui/separator"
+import { Truck, Calendar} from "lucide-react"
 
 export function AppSidebar() {
+    const sidebarItems = [
+        {
+            title: "Tournées",
+            url: "#",
+            icon: Truck
+        },
+        {
+            title: "Calendriers de livraison",
+            url: "#",
+            icon: Calendar
+        }
+    ]
     return (
       <Sidebar>
         <SidebarHeader>
             <div className="flex flex-row gap-2 items-center">
-                <Image src={logoCocagne} alt={"Réseau Cocagne"} width={50} height={50}></Image>
+                <Image src={logoCocagne} alt={"Réseau Cocagne"} width={80} height={80}></Image>
                 Réseau Cocagne
             </div>
         </SidebarHeader>
         <Separator />
         <SidebarContent>
-          <SidebarGroup />
-          <SidebarGroup />
+          <SidebarGroup>
+            <SidebarGroupLabel>Application</SidebarGroupLabel>
+            <SidebarGroupContent>
+                <SidebarMenu>
+                    {sidebarItems.map((item) => (
+                        <SidebarMenuItem key={item.title}>
+                        <SidebarMenuButton asChild>
+                            <a href={item.url}>
+                            <item.icon />
+                            <span>{item.title}</span>
+                            </a>
+                        </SidebarMenuButton>
+                        </SidebarMenuItem>
+                    ))}
+                </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+          
         </SidebarContent>
         <SidebarFooter />
       </Sidebar>

54180f82d66d661138adf09bc194608f01c03486
WiroWik <wirowik67@gmail.com>
2025-01-13 00:17:05 +0100

    Refactor database connection and remove Dockerfile; implement user retrieval in Home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,42 @@
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import { Table, TableBody, TableCaption, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import { getUser } from "@/db/index";
 
-export default function Home() {
+export default async function Home() {
+  const user = await getUser();
   return (
     <>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         SAE 5 : Jardin de Cocagne
       </h1>
+      <Separator className="my-5" />
       <Card>
         <CardHeader>
           <CardTitle>Informations</CardTitle>
           <CardDescription>Affichage pour le développement</CardDescription>
         </CardHeader>
         <CardContent>
-          <p>Card Content</p>
+          <Table>
+            <TableCaption>A list of your recent invoices.</TableCaption>
+            <TableHeader>
+              <TableRow>
+                <TableHead className="w-[100px]">Invoice</TableHead>
+                <TableHead>ID</TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Email</TableHead>
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {user.map((u) => (
+                <TableRow key={u.id}>
+                  <TableCell>{u.id}</TableCell>
+                  <TableCell>{u.name}</TableCell>
+                  <TableCell>{u.email}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
         </CardContent>
         <CardFooter>
           <p>Card Footer</p>
--- db/index.ts
+++ db/index.ts
@@ -1,4 +1,34 @@
 import 'dotenv/config';
 import { drizzle } from 'drizzle-orm/node-postgres';
-
+import { usersTable } from '@/db/schema';
+  
 const db = drizzle(process.env.DATABASE_URL!);
+
+async function main() {
+  const user: typeof usersTable.$inferInsert = {
+    name: 'John',
+    email: 'john@example.com'
+  };
+
+  await db.insert(usersTable).values(user);
+  console.log('New user created!')
+
+  const users = await db.select().from(usersTable);
+  console.log('Getting all users from the database: ', users)
+  /*
+  const users: {
+    id: number;
+    name: string;
+    age: number;
+    email: string;
+  }[]
+  */
+}
+
+export async function getUser() {
+    const result = await db.select().from(usersTable);
+    return result
+}
+
+main();
+

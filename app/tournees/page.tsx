77c3492b6dabd8e257c0daca8e56abd345699e46
Leon LeBerre <leon.le-berre5@etu.univ-lorraine.fr>
2025-01-14 12:03:04 +0100

    Add Leaflet map integration to Tournees component for enhanced delivery visualization

--- app/tournees/page.tsx
+++ app/tournees/page.tsx
@@ -1,9 +1,33 @@
+'use client'
+import { Separator } from "@/components/ui/separator";
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css';
+
 export default function Tournees() {
+    
+   
+
     return (
         <>
             <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
                 Tournées de livraison
             </h1>
+            <Separator className="my-5" />
+            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                />
+                <Marker position={[51.505, -0.09]}>
+                    <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+
+
+
+            
         </>
     );
 }

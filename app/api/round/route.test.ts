c2808f6d6e1fcb657dfab5ef89983b327733d410
WiroWik <wirowik67@gmail.com>
2025-01-23 00:16:59 +0100

    Remove unused jest-fetch-mock and promise-polyfill from package-lock.json

---
+++ app/api/round/route.test.ts
@@ -0,0 +1,13 @@
+/**
+ * @jest-environment node
+ */
+import { GET } from './route';
+
+it('should return data with status 200', async () => {
+  const response = await GET();
+  const body = await response.json();
+
+  expect(response.status).toBe(200);
+});
+
+
